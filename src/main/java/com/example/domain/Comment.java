eda3a06ad629dba9bbc978d413ddddf879dbec31
fujiiyuuki <yuki.fujii@rakus-partners.co.jp>
2020-02-12 14:36:37 +0900

    コメントのドメインクラスを作成

--- src/main/java/com/example/domain/Comment.java
+++ src/main/java/com/example/domain/Comment.java
@@ -1,5 +1,47 @@
 package com.example.domain;
 
+/**
+ * 記事へのコメントに関する情報を表すドメインクラス.
+ * 
+ * @author yuuki
+ *
+ */
 public class Comment {
+	/** 主キー */
+	private Integer id;
+	
+	/** 投稿者名 */
+	private String name;
+	
+	/** 投稿内容 */
+	private String content;
+	
+	/** 記事のID */
+	private Integer articleId;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Integer getArticleId() {
+		return articleId;
+	}
+	public void setArticleId(Integer articleId) {
+		this.articleId = articleId;
+	}
 
 }
